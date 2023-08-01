4246b653c9f4a8270d557766a2ef5ac207149252
马琪斌 <maqi1520@163.com>
2023-08-01 13:23:48 +0800

    feat: 🎸 ask-ai

---
+++ src/pages/api/generate.js
@@ -0,0 +1,36 @@
+import { OpenAIStream } from '../../utils/OpenAIStream'
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('Missing env var from OpenAI')
+}
+
+export const config = {
+  runtime: 'edge',
+}
+
+const handler = async (req) => {
+  const { messages } = await req.json()
+
+  console.log(messages)
+  if (!messages) {
+    return new Response('No messages in the request', { status: 400 })
+  }
+
+  const payload = {
+    // model: "text-davinci-003",
+    model: 'gpt-3.5-turbo',
+    messages,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 500,
+    stream: true,
+    n: 1,
+  }
+
+  const stream = await OpenAIStream(payload)
+  return new Response(stream)
+}
+
+export default handler
